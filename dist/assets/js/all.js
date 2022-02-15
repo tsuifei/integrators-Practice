8996469db5ace55def6bbf1406aedecd38cd5637
tsuifei <tsuifei@gmail.com>
2022-02-15 11:05:46 +0100

    [modify] carrousel bloc

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,6 +1,20 @@
-// console.log('Hello!');
-// $(document).ready(() => {
-//   console.log('Hello!');
-// });
 "use strict";
+
+// carrousel
+var items = document.querySelectorAll('.carousel .carousel-item');
+items.forEach(function (el) {
+  var minPerSlide = 3;
+  var next = el.nextElementSibling;
+
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
+    }
+
+    var cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
 //# sourceMappingURL=all.js.map
